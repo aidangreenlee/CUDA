d7294d2a0177d58f11e54a0dca1384ad9746504d
aidangreenlee <aidangreenlee@gmail.com>
2020-11-01 13:42:48 -0500

    timing

--- vectorCPU.cpp
+++ vectorCPU.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
+#include <chrono>
 #include <math.h>
 
 void add(int n, float *x, float *y, float *z){
     for(int i = 0; i < n; i++){
         z[i] = x[i] + y[i];
-    }
-}
-
-void dot(int n, float *x, float *y, float *c){
-    for(int i = 0; i < n; i++){
-        *c += x[i] * y[i];
     }
 }
 
@@ -19,22 +14,27 @@ int main(void){
     float *x = new float[N];
     float *y = new float[N];
     float *z = new float[N];
-    float *c = new float;
 
     for(int i = 0; i < N; i++){
         x[i] = 1;
         y[i] = 1;
     }
 
+    auto start = std::chrono::high_resolution_clock::now();
     add(N, x, y, z);
-    dot(N, x, y, c);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    double time_taken =  std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); 
+    time_taken *= 1e-9;
 
     float maxError = 0;
     for(int i = 0; i < N; i++){
         maxError = fmax(maxError, fabs(z[i]) - 3);
     }
     std::cout << "Error: " << maxError << std::endl;
-    std::cout << "dot product: " << *c << std::endl;
+    std::cout << "Time taken by program is : " << time_taken; 
+    std::cout << " sec" << std::endl; 
+
     delete [] x;
     delete [] y;
 
